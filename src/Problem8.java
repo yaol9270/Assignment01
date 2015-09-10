8fce19943562bda071aecf37a599abb2412dd972
yaol9270 <yaol9270@HRH-IC0027328.wrdsb.ca>
2015-09-10 11:18:05 -0400

    Diid 1-8

---
+++ src/Problem8.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yaol9270
+ */
+public class Problem8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+    }
+}
